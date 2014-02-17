9f5a6d842b3865f405ddb0124939df04df416d35
mustafatasli <mustafaxtasli@gmail.com>
2014-02-17 14:03:59 +0200

    dump fix

--- src/main/java/net/mustafatasli/db/BTree.java
+++ src/main/java/net/mustafatasli/db/BTree.java
@@ -3,11 +3,12 @@ package net.mustafatasli.db;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
 
 public class BTree<K extends Comparable<K>, V> implements Map<K, V> {
-    private final int ORDER = 6;
+    private final int ORDER = 8;
     private int height = 0;
     private Node root;
     private NodeManager nm = new NodeManager();
@@ -68,6 +69,7 @@ public class BTree<K extends Comparable<K>, V> implements Map<K, V> {
 
     }
 
+    @SuppressWarnings("unchecked")
     public V put(K key, V value) {
         Node node = insert(root, key, value, height);
         if (node != null) {
@@ -96,6 +98,7 @@ public class BTree<K extends Comparable<K>, V> implements Map<K, V> {
         return right;
     }
 
+    @SuppressWarnings("unchecked")
     private Node insert(Node node, K key, V value, int height) {
         int index;
         int child = -1;
@@ -112,6 +115,9 @@ public class BTree<K extends Comparable<K>, V> implements Map<K, V> {
             for (index = 0; index < node.count; index++) {
                 if ((index + 1 == node.count) || key.compareTo((K) node.keys[index + 1]) < 0) {
                     Node n = insert(this.nm.getNode(node.children[index++]), key, value, --height);
+                    if (key.compareTo((K) node.keys[0]) < 0) {
+                        node.keys[0] = key;
+                    }
                     if (n == null) {
                         return null;
                     }
@@ -140,20 +146,17 @@ public class BTree<K extends Comparable<K>, V> implements Map<K, V> {
     }
 
     public void dump() {
-        dump(this.root);
+        List<Node> nodes = new ArrayList<Node>();
+        nodes.add(this.root);
+        dump(nodes);
     }
 
     @SuppressWarnings("unchecked")
-    private void dump(Node node) {
-        List<Node> nodes = new ArrayList<Node>();
-        
-        for(long index: node.children) {
-            if (index != -1) {
-                nodes.add(this.nm.getNode(index));
-            }  
-        }
-        
+    private void dump(List<Node> nodes) {
         K key;
+        long child;
+        List<Node> children = new ArrayList<Node>();
+        
         for(Node n: nodes) {
             System.out.println(n.index);
             for(int i = 0; i < n.count; i++) {
@@ -161,10 +164,21 @@ public class BTree<K extends Comparable<K>, V> implements Map<K, V> {
                 if (key != null) {
                    System.out.print(key.toString() + ", ");
                 } 
+                
+                child = n.children[i];
+                if (child != -1) {
+                    children.add(this.nm.getNode(child));
+                }  
             }
+            
             System.out.println();
         }
         
+        if(children.size() > 0) {
+            this.dump(children); 
+        } else {
+            return;
+        }
     }
 
     private class NodeManager {
@@ -188,7 +202,6 @@ public class BTree<K extends Comparable<K>, V> implements Map<K, V> {
     }
 
     private class Node {
-        private final int ORDER = 6;
         private final long index;
         private int count = 0;
 
